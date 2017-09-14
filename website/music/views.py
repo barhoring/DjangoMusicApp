eb9c03b2dbf3708d3fd2dc62194a7049b365a87d
Bar Bar <bar.apple@gmail.com>
2017-09-14 09:18:16 +0300

    first commit - lesson 13 completed

---
+++ website/music/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Album
+
+def index(request):
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id)+'/'
+        html += '<a href="'+ url +'">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id :" + str(album_id) + "</h2>")
+
+
+
+# Create your views here.
+
+

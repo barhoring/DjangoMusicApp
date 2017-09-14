097f264220630755806af8c8f18295d61a348c04
Bar Bar <bar.apple@gmail.com>
2017-09-14 11:23:16 +0300

    lesson 16 completed

--- website/music/views.py
+++ website/music/views.py
@@ -1,17 +1,19 @@
+from django.http import Http404
+
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id)+'/'
-        html += '<a href="'+ url +'">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id :" + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(id = album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album})
+
 
 
 
